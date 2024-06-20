cfb25da6ac789030c428c96c11e74f2b172cc9f1
Vladimir Mitrev <orionsbelt1017@gmail.com>
2024-06-20 11:53:53 +0300

    Add register and login user

--- routes.js
+++ routes.js
@@ -1,10 +1,10 @@
 const router = require('express').Router();
 const homeController = require('./controllers/homeController');
-// const authController = require('./controllers/authController');
+const authController = require('./controllers/authController');
 // const volcanoController = require('./controllers/volcanoController');
 
 router.use(homeController);
-// router.use('/auth', authController);
+router.use('/auth', authController);
 // router.use('/volcano', volcanoController);
 
 router.all('*', (req, res) => {
